54279db8c1a83d6d967bd978377c3ad2c3dc1b29
Kevin Salazar <kevinqasdfgz@gmail.com>
2025-07-19 14:47:24 -0600

    s

--- data/data.py
+++
@@ -1,6 +0,0 @@
-urban_routes_url = 'https://cnt-abd98e45-f861-4932-a827-6b83dbc1e70f.containerhub.tripleten-services.com/?lng=es'
-address_from = 'East 2nd Street, 601'
-address_to = '1300 1st St'
-phone_number = '+1 123 123 12 12'
-card_number, card_code = '1234 5678 9100', '111'
-message_for_driver = 'Muéstrame el camino al museo'
